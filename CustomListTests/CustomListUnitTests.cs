751382b1e8e14676f0a4bae21affd3281141ed55
unknown <meekhunsutp@gmail.com>
2020-10-26 14:20:09 -0500

    Created Test Methods for Add and Remove Methods

--- CustomListTests/CustomListUnitTests.cs
+++ CustomListTests/CustomListUnitTests.cs
@@ -7,6 +7,8 @@ namespace CustomListTests
     [TestClass]
     public class CustomListUnitTests
     {
+
+        //ADD METHOD
         [TestMethod]
         public void Add_AddItemToEmptyList_ItemGoesToIndexZero()
         {
@@ -23,7 +25,7 @@ namespace CustomListTests
             // Assert
             Assert.AreEqual(expected, actual);
         }
-        
+
         [TestMethod]
         public void Add_AddItemToEmptyList_CountIncrementsToOne()
         {
@@ -40,6 +42,213 @@ namespace CustomListTests
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Add_AddItemToPopulatedList_ItemGoesToLastIndex()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int item = 10;
+            testList.Add(2);
+            testList.Add(4);
+            testList.Add(4);
+            testList.Add(6);
+            int expected = 10;
+            int actual;
+
+            // Act
+            testList.Add(item);
+            actual = testList[4]; // error expected until "indexer property" is added to class
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Add_AddItemToPopulatedList_CountIncrementsByOne()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int item = 10;
+            testList.Add(2);
+            testList.Add(4);
+            testList.Add(4);
+            testList.Add(6);
+            int expected = 5;
+            int actual;
+
+            // Act
+            testList.Add(item);
+            actual = testList.Count; // error expected until "Count" is added to class
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Add_AddMoreItemsThanCurrentCapacity_CapacityDoubles()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            int item = 10;
+            testList.Add(2);
+            testList.Add(4);
+            testList.Add(4);
+            testList.Add(6);
+            int expected = 8;
+            int actual;
+
+            // Act
+            testList.Add(item);
+            actual = testList.Capacity; // error expected until "Count" is added to class
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        //REMOVE METHODS
+        [TestMethod]
+        public void Remove_RemoveItemFromList_FourGoesToIndexZero()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(2);
+            testList.Add(4);
+            testList.Add(4);
+            testList.Add(6);
+            int item = 2;
+            int expected = 4;
+            int actual;
+            // Act
+            testList.Remove(item);
+            actual = testList[0]; // error expected until "indexer property" is added to class
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemoveNonExistingItem_CountRemainsTheSame()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(2);
+            testList.Add(4);
+            testList.Add(4);
+            testList.Add(6);
+            int item = 3;
+            int expected = 4;
+            int actual;
+            // Act
+            testList.Remove(item);
+            actual = testList.Count(); // error expected until "Count" is added to class
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemoveItemOnceFromList_SixAtIndexTwo()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(2);
+            testList.Add(4);
+            testList.Add(4);
+            testList.Add(6);
+            int item = 4;
+            int expected = 6;
+            int actual;
+            // Act
+            testList.Remove(item);
+            actual = testList[2]; // error expected until "indexer property" is added to class
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemoveNumberTwoFromList_CountDecrementsByOne()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(2);
+            testList.Add(4);
+            testList.Add(4);
+            testList.Add(6);
+            int item = 2;
+            int expected = 3;
+            int actual;
+            // Act
+            testList.Remove(item);
+            actual = testList.Count; // error expected until "Count" is added to class
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemoveAllFromList_CountOfZero()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(2);
+            testList.Add(4);
+            testList.Add(4);
+            testList.Add(6);
+            int expected = 0;
+            int actual;
+            // Act
+            testList.Remove(2);
+            testList.Remove(4);
+            testList.Remove(4);
+            testList.Remove(6);
+            actual = testList.Count; // error expected until "Count" is added to class
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }  
+        [TestMethod]
+        public void Remove_RemoveOneOfTwoSameItemFromList_CountOfOneItem()
+        {
+            // Arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(2);
+            testList.Add(4);
+            testList.Add(4);
+            testList.Add(6);
+            int expected = 1;
+            int actual;
+            // Act
+            testList.Remove(4);
+            actual = testList.Count(4); // error expected until "Count" is added to class
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+        //[TestMethod]
+        //public void Find_FindFirstIndexOf4_IndexOf1()
+        //{
+        //    // Arrange
+        //    CustomList<int> testList = new CustomList<int>() { 2, 4, 4, 6 };
+        //    int expected = 1;
+        //    int actual;
+
+        //    // Act
+        //    actual = testList.IndexOf(4); // error expected until "Count" is added to class
+
+        //    // Assert
+        //    Assert.AreEqual(expected, actual);
+        //}
+        
+        //[TestMethod]
+        //public void Find_FindLastIndexOf4_IndexOf2()
+        //{
+        //    // Arrange
+        //    CustomList<int> testList = new CustomList<int>() { 2, 4, 4, 6 };
+        //    int expected = 1;
+        //    int actual;
+
+        //    // Act
+        //    actual = testList.IndexOf(4); // error expected until "Count" is added to class
+
+        //    // Assert
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+
 
         // what if i .Add to a list that has a couple things in it already (position of item)?
         // what if i .Add to a list that has a couple things in it already (value of Count)?
--- CustomListUnitTestStarter/CustomList.cs
+++ CustomListUnitTestStarter/CustomList.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace CustomListUnitTestStarter
 {
-    public class CustomList<T>
+    public class CustomList<T> //T = int
     {
-        public void Add(T item)
+        //Global Member Variables
+        int count;
+        int capacity;
+        T[] items;
+
+        //Properties
+        //count
+        Count get
+        //capacity
+        capacity get set 
+
+        public CustomList()
         {
 
         }
+
+        public void Add(T item)
+        {
+            //logic to "add" to my items array//what are the steps to add item? increase capacity? 
+            //take item and add to next available index
+            //1.
+            //2.
+        }
     }
 }
+//array loops if statements only
+//indexer??
+//ToString
